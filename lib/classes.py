545291d4887346187cc5cbd85d5cf10b5186b8ea
MapacheTech <rosasmm01@gmail.com>
2024-01-26 01:38:04 -0600

    Actualizacion 25 enero

--- lib/classes.py
+++ lib/classes.py
@@ -6,24 +6,29 @@ class estudiante():
         self.calificacion = []
         pass
     
+    
     def setCalificacion(self, calificacion):
         self.calificacion.append(calificacion)
-        pass
+        return calificacion
     
     def promedio(self):
-        pass
+        promedio = sum(self.calificacion)/len(self.calificacion)
+        return promedio
     
     def __str__(self):
-        return
+        return f"Matricula: {self.matricula}\nNombre: {self.apellidoNombre}\nEdad: {self.edad}\nCalificaciones: {self.calificacion}"
 class estudianteGraduado(estudiante):
     def __init__(self, matricula, apellidoNombre, edad, fecha, tesis):
         super().__init__(matricula, apellidoNombre, edad)
         self.fecha = fecha
         self.tesis = tesis
         pass
-    def chkGrad():
-        
+    def chkGrad(self):
+        promedio = self.promedio()
+        if promedio <= 7:
+            print("\033[31m"+ str(promedio) + "\033[0m")
+        else:
+            print(promedio)
         pass
     
-    
-    pass
+
---
+++ main.py
@@ -0,0 +1,11 @@
+from lib import *
+
+estudiante_1 = estudianteGraduado(601049, "ACOSTA ZAIRA", 35, "01/07/13", "Los neutrinos saben a jugo de uva")
+estudiante_1.setCalificacion(int(10))
+estudiante_1.setCalificacion(int(9))
+estudiante_1.setCalificacion(int(8))
+estudiante_1.setCalificacion(int(7))
+estudiante_1.setCalificacion(int(6))
+print(estudiante_1)
+
+print(estudiante_1.chkGrad())
