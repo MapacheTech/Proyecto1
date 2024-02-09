756eab3553a4792dca6ac8b10269d91b7d8c7a48
MapacheTech <rosasmm01@gmail.com>
2024-02-09 13:41:52 -0600

    Actualizacion 9 febrero

--- lib/classes.py
+++ lib/classes.py
@@ -12,23 +12,61 @@ class estudiante():
         return calificacion
     
     def promedio(self):
-        promedio = sum(self.calificacion)/len(self.calificacion)
+        ZeroDivisionError
+        try:
+            promedio = sum(self.calificacion)/len(self.calificacion)
+        except Exception as e:
+            print(f"Error: {e}")    
         return promedio
     
     def __str__(self):
-        return f"Matricula: {self.matricula}\nNombre: {self.apellidoNombre}\nEdad: {self.edad}\nCalificaciones: {self.calificacion}"
+        return f"Matricula: {self.matricula} Nombre: {self.apellidoNombre} Edad: {self.edad} Calificaciones: {self.calificacion}"
 class estudianteGraduado(estudiante):
-    def __init__(self, matricula, apellidoNombre, edad, fecha, tesis):
+    def __init__(self, matricula, apellidoNombre, edad):
         super().__init__(matricula, apellidoNombre, edad)
-        self.fecha = fecha
-        self.tesis = tesis
         pass
     def chkGrad(self):
         promedio = self.promedio()
-        if promedio <= 7:
-            print("\033[31m"+ str(promedio) + "\033[0m")
+        graduado = ""
+        if promedio <= 5.9:
+            graduado = "No"
         else:
-            print(promedio)
-        pass
+            graduado = "Si"
+        return graduado
     
-
+    def setFecha(self, fecha):
+        self.fecha = fecha
+        return fecha
+    
+    def chckFecha(self):
+        promedio = self.promedio()
+        
+        fechaGrad = ""
+        if promedio <= 5.9:
+            fechaGrad = "N/A"
+        else:
+            fecha = self.fecha
+            fechaGrad = fecha
+        return fechaGrad
+    
+    def setTesis(self, tesis):
+        self.tesis = tesis
+        return tesis
+    
+    def chckTesis(self):
+        promedio = self.promedio()
+        
+        tesisGrad = ""
+        if promedio <= 5.9:
+            tesisGrad = "N/A"
+        else:
+            tesis = self.tesis
+            tesisGrad = tesis
+        return tesisGrad
+    
+    def __str__(self):
+        promedio = self.promedio()
+        if promedio <= 5.9:
+            return f"Matricula: {self.matricula} Nombre Completo: {self.apellidoNombre} Edad: {self.edad} Estudiante No Graduado"
+        else:
+            return f"Matricula: {self.matricula} Nombre Completo: {self.apellidoNombre} Edad: {self.edad} Graduado el: {self.fecha} Con la Tesis: {self.tesis}"
